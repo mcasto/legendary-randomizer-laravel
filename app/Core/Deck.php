8707dc560be74548a924d78d02755cfcd30a2be2
Mike Casto <castoware@gmail.com>
2025-02-09 16:38:47 -0500

    Added placeholders for Deck & Setup

---
+++ app/Core/Deck.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Core;
+
+class Deck
+{
+ // this will contain setup & entities
+}
---
+++ app/Core/Setup.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Core;
+
+class Setup
+{
+ // this will contain the setup for the deck: how many schemes, masterminds, villains, henchmen, heroes, bystanders, and twists the deck will contain
+
+ // it starts with the default setup based on the number of players, then gets adjusted by Handlers
+}
